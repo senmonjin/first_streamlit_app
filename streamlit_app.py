14205eb90964a79e378515548e454bccad609ead
Israel Salcedo Jiménez <115989230+senmonjin@users.noreply.github.com>
2022-10-19 15:17:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Por favor selecciona una fruta sobre la que obtener información)  
+    streamlit.error("Por favor selecciona una fruta sobre la que obtener información")  
   else:
     fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
